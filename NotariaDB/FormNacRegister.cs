5b7bcbf561ed9e3b8ca9007b970503e692a7bc25
juansedo <jsdiazo@eafit.edu.co>
2020-11-03 01:14:20 -0500

    ComboBox loads information now by DataSource

--- NotariaDB/FormNacRegister.cs
+++ NotariaDB/FormNacRegister.cs
@@ -12,7 +12,7 @@ namespace NotariaDB
 {
     public partial class FormNacRegister : Form
     {
-        public Models.Nacimientos Register;
+        public Models.Nacimientos Register =new Models.Nacimientos();
 
         public FormNacRegister()
         {
@@ -23,43 +23,63 @@ namespace NotariaDB
         {
             using (Models.notariadbContext db = new Models.notariadbContext())
             {
-                cBloodtype.Items.Clear();
-                cBloodtype.Items.AddRange( (from s in db.Bloodtypes select s.Name).ToArray() );
+                initializeComboBox(cBloodtype, "Name", "BloodtypeId");
+                cBloodtype.DataSource = (from s in db.Bloodtypes select new { s.BloodtypeId, s.Name }).ToList();
+                cBloodtype.SelectedItem = null;
 
-                cDepartment.Items.Clear();
-                cDepartment.Items.AddRange( (from s in db.Departments select s.Name).ToArray() );
+                initializeComboBox(cDepartment, "Name", "Id");
+                cDepartment.DataSource = (from s in db.Departments select new {s.Id, s.Name}).ToArray();
+                cDepartment.SelectedIndex = 1;
 
-                cAttachtype.Items.Clear();
-                cAttachtype.Items.AddRange( (from s in db.NacAttaches select s.Name).ToArray() );
-                
-                cNotary.Items.Clear();
-                var result = (from n in db.Notarios join user in db.Usuarios on n.UserId equals user.Id select new {user.Name, user.Surname}).ToList();
-                foreach(var item in result)
-                    cNotary.Items.Add(item.Name + " " + item.Surname);
+                initializeComboBox(cAttachtype, "Name", "AttachId");
+                cAttachtype.DataSource = (from s in db.NacAttaches select new { s.AttachId, s.Name }).ToArray();
 
+                initializeComboBox(cNotary, "Name", "Id");
+                var result = (from n in db.Notarios
+                              join user in db.Usuarios
+                              on n.UserId equals user.Id
+                              select new {Id=n.UserId, Name=string.Concat(user.Name," ",user.Surname)}).ToList();
+                cNotary.DataSource = result;
             }
+        }
+
+        private void initializeComboBox(ComboBox cb, string display, string value)
+        {
+            cb.Items.Clear();
+            cb.DisplayMember = display;
+            cb.ValueMember = value;
         }
 
         private void btnAccept_Click(object sender, EventArgs e)
         {
             this.Register.Nuip = tNuip.Text;
             this.Register.Serial = tSerial.Text;
-            this.DialogResult = DialogResult.OK;
-            this.Close();
+            this.Register.Name = tName.Text;
+            this.Register.Surname1 = tSurname1.Text;
+            //this.Register.Birthdate
+            using (Models.notariadbContext db = new Models.notariadbContext()) {
+                this.Register.Bloodtype = db.Bloodtypes
+                                            .Single(c => c.Name == cBloodtype.Text);
+                //this.Register.Place = db.Places
+                //                            .Where(c => c. == cBloodtype.Text &&)
+                //                            .First();
+            }
+
+            //this.DialogResult = DialogResult.OK;
+            //this.Close();
         }
 
         private void cDepartment_SelectedIndexChanged(object sender, EventArgs e)
         {
             cCity.Items.Clear();
             cCity.Text = "";
-            string selection = cDepartment.SelectedItem.ToString();
+            int selection = Int32.Parse(cDepartment.SelectedValue.ToString());
             using (Models.notariadbContext db = new Models.notariadbContext())
             {
-                int box_id = db.Departments.Where(c => c.Name == selection).Select(c => c.Id).First(); 
                 cCity.Items.AddRange((from p in db.Places
                                       join c in db.Cities
                                       on p.CityId equals c.Id
-                                      where p.DepartmentId == box_id
+                                      where p.DepartmentId == selection
                                       select c.Name).ToArray());
             }
         }
