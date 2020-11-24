e45492a05a9ec21bf1fdf0e35fcd7ccfa6616a47
Juan Sebastián Díaz Osorio <52968530+juansedo@users.noreply.github.com>
2020-11-24 00:49:11 -0500

    Update Program.cs

--- NotariaDB/Program.cs
+++ NotariaDB/Program.cs
@@ -18,7 +18,7 @@ namespace NotariaDB
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new formMain());
+            Application.Run(new Views.formMain());
         }
     }
 }
