541dc92e2c1f7efa85bc1fb9f78f451d48bafec3
David Calle <dcalleg@eafit.edu.co>
2020-11-23 20:59:49 -0500

    Initial files

---
+++ NotariaDB/Database/Migrations/20201124003209_test5.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NotariaDB.Database.Migrations
+{
+    public partial class test5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "usuarios",
+                columns: new[] { "id", "birth_date", "doctype_id", "expedition_date", "expedition_place", "name", "surname" },
+                values: new object[,]
+                {
+                    { "10397604", new DateTime(1992, 9, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, new DateTime(2005, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), " SALGAR", " ANA CRISTINA", " DÍAZ OSORIO" },
+                    { "1039760194", new DateTime(1992, 9, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, new DateTime(2005, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "SALGAR", "ANA CRISTINA", "DÍAZ OSORIO" },
+                    { "21979476", new DateTime(1970, 5, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "SALGAR", "RUTH NELLY", "OSORIO GARCÍA" },
+                    { "719912", new DateTime(1942, 12, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "SALGAR", "VILEY DE JESÚS", "DÍAZ GARCÍA" },
+                    { "98509595", new DateTime(1970, 5, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "SALGAR", "JAVIER ALONSO", "DÍAZ GALLEGO" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: "1039760194");
+
+            migrationBuilder.DeleteData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: "10397604");
+
+            migrationBuilder.DeleteData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: "21979476");
+
+            migrationBuilder.DeleteData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: "719912");
+
+            migrationBuilder.DeleteData(
+                table: "usuarios",
+                keyColumn: "id",
+                keyValue: "98509595");
+        }
+    }
+}
---
+++ NotariaDB/Utilities/Commands/deleteCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotariaDB.Utilities.Commands
+{
+    class Class1
+    {
+    }
+}
